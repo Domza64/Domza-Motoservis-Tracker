535ebe73cd754dacf1ef181e2d40bd41fa18c9a6
Domza64 <juricicdominik@protonmail.com>
2024-09-28 15:44:39 +0200

    Im stupid - actually fix chart.js build error

--- src/app/dashboard/[id]/data/page.tsx
+++ src/app/dashboard/[id]/data/page.tsx
@@ -1,9 +1,7 @@
-import LineChart from "@/component/dashboard/LineChart";
-
 export default function DataPage() {
   return (
     <section className="py-12 w-full">
-      <LineChart />
+      <span>Data</span>
     </section>
   );
 }
